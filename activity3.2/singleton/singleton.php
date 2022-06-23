9cd15889cefd0499fbcd8010323fc821576a0a33
Emna Ghariani <eghariani@dom.tti>
2022-06-23 18:43:26 +0100

    added singleton

--- activity3.2/singleton/singleton.php
+++ activity3.2/singleton/singleton.php
@@ -1,17 +1,47 @@
 <?php
 
-class SingleTon
-{
-    public static $instance;
-
-    public function __construct()
+class ConnectDb {
+    // Hold the class instance.
+    private static $instance = null;
+    private $conn;
+    
+  
+    // The db connection is established in the private constructor.
+    private function __construct()
     {
+        $this->conn = $this->getDb();
+
+    }
+    // Magic method clone is empty to prevent duplication of connection
+    private function __clone(){}
+
+    private function getDb(){
+        try
+        {
+            $conn = new PDO('mysql:host=localhost;dbname=emnadatabase;charset=utf8', 'root', '');
+        }
+        catch (Exception $e)
+        {
+            die('Erreur : ' . $e->getMessage());
+        } 
+        return $conn;
     }
 
-    public function getInstance() {
-        if($instance === null) {
-            $instance = new SingleTon();
-        }
-        return $instance;
+
+    public static function getInstance()
+    {
+      if(!self::$instance)
+      {
+        self::$instance = new ConnectDb();
+      }
+     
+      return self::$instance;
     }
-}
+    
+    public function getConnection()
+    {
+      return $this->conn;
+    }
+  }
+  
+
